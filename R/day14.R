706a2604f3a07a7cbc2f726f808878687deda319
TJ Mahr <mahr@wisc.edu>
2017-12-18 20:51:23 -0600

    pass day 14

--- R/day14.R
+++ R/day14.R
@@ -41,3 +41,81 @@ binary_to_grid <- function(xs) {
   bits <- ifelse(chars == "1", "#", ".")
   paste0(bits, collapse = "")
 }
+
+
+#' @export
+count_grid_regions <- function(bits) {
+  # Get coordinates of next "one" in matrix
+  find_next_one <- function(grid, width = 128) {
+    ones <- which(grid == "one")[1]
+    locate_cell_position(grid, ones)
+  }
+
+  # Get the row, column of the nth item in a matrix
+  locate_cell_position <- function(matrix, position) {
+    col <- ((position - 1) %/% ncol(matrix)) + 1
+    row <- ((position - 1) %% nrow(matrix)) + 1
+    c(row, col)
+  }
+
+  # Set all adjacent "one"s to the same value
+  flood_fill <- function(grid, start, value) {
+    cells_to_check <- list(start)
+
+    while (length(cells_to_check) > 0) {
+      grid <- mark_cell(grid, cells_to_check[[1]], value)
+      adjacent_cells <- find_neighboring_ones(grid, cells_to_check[[1]])
+      cells_to_check <- unique(c(cells_to_check, adjacent_cells))
+      cells_to_check[[1]] <- NULL
+    }
+
+    grid
+  }
+
+  # Find neighbors with a sentinel "one" value
+  find_neighboring_ones <- function(grid, cell) {
+    adjacent <- get_neighbors(cell)
+    values <- adjacent %>% lapply(look_up_cell, grid) %>% unlist()
+    adjacent[values == "one"]
+  }
+
+  # Get a cell's value
+  look_up_cell <- function(cell, grid) {
+    grid[cell[1], cell[2]]
+  }
+
+  # Update a cell
+  mark_cell <- function(grid, cell, value) {
+    grid[cell[1], cell[2]] <- value
+    grid
+  }
+
+  # Get indices of a cell's neigbors
+  get_neighbors <- function(cell, width = 128) {
+    x <- c(-1,  0, 0, 1)
+    y <- c( 0, -1, 1, 0)
+    xs <- x + cell[1]
+    ys <- y + cell[2]
+    bad_x <- which(xs <= 0 | width < xs)
+    bad_y <- which(ys <= 0 | width < ys)
+    if (length(c(bad_x, bad_y)) != 0) {
+      xs <- xs[-c(bad_x, bad_y)]
+      ys <- ys[-c(bad_x, bad_y)]
+    }
+    Map(function(x, y) c(x, y), xs, ys)
+  }
+
+  chars <- bits %>%
+    strsplit("") %>%
+    unlist()
+
+  grid <- matrix(chars, nrow = 128, byrow = TRUE)
+  grid[grid == "1"] <- "one"
+
+  seed <- 1
+  while ("one" %in% grid) {
+    grid <- flood_fill(grid, find_next_one(grid), seed)
+    seed <- seed + 1
+  }
+  max(as.numeric(grid))
+}
